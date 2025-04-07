efbd4d674cae839748b307b4b96dbe5e16c32de8
Kasra Rasaee <krasaee@gmail.com>
2025-04-07 14:22:18 -0700

    update to use str2bool util function for enable local auth

--- environment.py
+++ environment.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import dotenv
+from ismcore.utils.general_utils import str2bool
 from ismdb.postgres_storage_class import PostgresDatabaseStorage
 
 dotenv.load_dotenv()
@@ -12,7 +13,7 @@ API_ROOT_PATH = os.environ.get("API_ROOT_PATH", None)
 ROUTING_FILE = os.environ.get("ROUTING_FILE", '.routing.yaml')
 FIREBASE_CREDENTIALS_JSON_FILE = os.environ.get("FIREBASE_CREDENTIALS_JSON_FILE", ".firebase-credentials.json")
 LOG_LEVEL = os.environ.get("LOG_LEVEL", logging.INFO)
-ENABLED_LOCAL_AUTH = os.environ.get("ENABLED_LOCAL_AUTH", False)
+ENABLED_LOCAL_AUTH = str2bool(os.environ.get("ENABLED_LOCAL_AUTH", "False"))
 ENABLED_FIREBASE_AUTH = os.environ.get("ENABLED_FIREBASE_AUTH", False)
 
 if not DATABASE_URL:
@@ -22,7 +23,7 @@ logging.basicConfig(level=LOG_LEVEL)
 logging.info(DATABASE_URL[:20])
 logging = logging.getLogger(__name__)
 
-# setup the storage device for managing state, state configs, templates, models and so forth
+# set up the storage device for managing state, state configs, templates, models and so forth
 storage = PostgresDatabaseStorage(database_url=DATABASE_URL)
 
 
