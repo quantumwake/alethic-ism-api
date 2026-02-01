44ee810ffd87d4fe975fe86e3bd68f084c80e092
Kasra Rasaee <krasaee@gmail.com>
2026-01-31 16:51:57 -0800

    added edge function update functionality for routes (aka processor_states)

--- api/processor_state_route.py
+++ api/processor_state_route.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from fastapi import APIRouter
 from ismcore.messaging.base_message_route_model import RouteMessageStatus
-from ismcore.model.base_model import ProcessorState
+from ismcore.model.base_model import ProcessorState, EdgeFunctionConfig
 from pydantic import ValidationError
 
 from environment import storage
@@ -21,8 +21,7 @@ state_router_route = message_router.find_route(SELECTOR_STATE_ROUTER)
 
 @processor_state_router.post("")
 @check_null_response
-async def insert_processor_state_route(processor_state: ProcessorState) \
-        -> Optional[ProcessorState]:
+async def insert_processor_state_route(processor_state: ProcessorState) -> Optional[ProcessorState]:
     return storage.insert_processor_state_route(
         processor_state=processor_state
     )
@@ -50,3 +49,14 @@ async def execute_processor_state_route(route_id: str) -> RouteMessageStatus:
     message_string = json.dumps(message)
     status = await state_router_route.publish(message_string)
     return status
+
+
+@processor_state_router.get('/{route_id}/edge-function')
+async def get_edge_function_config(route_id: str) -> Optional[EdgeFunctionConfig]:
+    return storage.fetch_edge_function_config(route_id=route_id)
+
+
+@processor_state_router.put('/{route_id}/edge-function')
+@check_null_response
+async def update_edge_function_config(route_id: str, config: EdgeFunctionConfig) -> Optional[EdgeFunctionConfig]:
+    return storage.update_edge_function_config(route_id=route_id, config=config)
